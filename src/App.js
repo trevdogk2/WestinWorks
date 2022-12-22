506b65a7dc5fc31d730586570636075073fca26a
Trevor Westin <trevorwwestin@gmail.com>
2022-12-21 23:30:48 -0500

    hello world

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>weird</code>
+          <code>Hello World!</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          YOOOOO
-        </a>
       </header>
     </div>
   );
